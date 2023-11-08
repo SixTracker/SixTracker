06e3f7bebdd33b2d24357006aae6bba3ad5cd915
Chris <christian.bellei@sptech.school>
2023-11-08 12:41:10 -0300

    Atualizando a MedidaModel para a preparação das dashs

--- Site/src/models/medidaModel.js
+++ Site/src/models/medidaModel.js
@@ -66,3 +66,67 @@ module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal
 }
+
+/*
+var database = require("../database/config");
+
+function buscarUltimasMedidas() {
+
+    
+    instrucaoSql = ''
+
+    // if (process.env.AMBIENTE_PROCESSO == "producao") {
+    //     instrucaoSql = `select top ${limite_linhas}
+    //     dht11_temperatura as temperatura, 
+    //     dht11_umidade as umidade,  
+    //                     momento,
+    //                     FORMAT(momento, 'HH:mm:ss') as momento_grafico
+    //                 from medida
+    //                 where fk_aquario = ${idUsuario}
+    //                 order by id desc`;
+// }
+     if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select count(joel) as Joel, count(ellie) as Ellie, count(bill) as Bill, count(infectados) as Infectados, count(tess) as Tess from personagens join usuario on usuario.id = fkusuario;
+       `;
+       
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal() {
+
+    instrucaoSql = ''
+
+    // if (process.env.AMBIENTE_PROCESSO == "producao") {
+    //     instrucaoSql = `select top 1
+    //     dht11_temperatura as temperatura, 
+    //     dht11_umidade as umidade,  
+    //                     CONVERT(varchar, momento, 108) as momento_grafico, 
+    //                     fk_aquario 
+    //                     from medida where fk_aquario = ${idUsuario} 
+    //                 order by id desc`;
+
+    // } 
+     if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select count(joel) as Joel, count(ellie) as Ellie, count(bill) as Bill, count(infectados) as Infectados, count(tess) as Tess from personagens join usuario on usuario.id = fkusuario;
+        `;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+}
+*/
