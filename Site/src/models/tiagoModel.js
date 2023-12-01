021dc09f89d8fd433b35ff8452cbe4407d483c41
ChrisMiguel <christianbellei84@gmail.com>
2023-12-01 11:01:19 -0300

    Atualizando o tiagoModel

--- Site/src/models/tiagoModel.js
+++ Site/src/models/tiagoModel.js
@@ -5,12 +5,16 @@ var database = require("../database/config");
 
 function tempoRealTempCPU(idServidor) {
     var instrucaoSql = `
-    SELECT 
-    valorRegistro,
-    DATE_FORMAT(dataRegistro, '%H:%i') AS horaRegistro
-FROM registro
-ORDER BY dataRegistro DESC
-LIMIT 1;
+	SELECT TOP 1 valorRegistro, FORMAT(dataRegistro, 'HH:mm') AS dataRegistro
+FROM Registro
+WHERE
+    fkComponente IN (
+        SELECT idComponente
+        FROM componente
+        WHERE fkServidor = 12 AND nome = 'Temperatura CPU'
+    )
+ORDER BY dataRegistro DESC;
+
     `;
 
     console.log("Executando a instrução SQL no tiagoModel: \n" + instrucaoSql);
