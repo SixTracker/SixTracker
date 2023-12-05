f2078eff8466e1b37e0be6095381dbbab36c18a8
ChrisMiguel <christianbellei84@gmail.com>
2023-12-05 16:27:40 -0300

    Atualização 41.0

--- Site/src/models/graficosLiviaModel.js
+++ Site/src/models/graficosLiviaModel.js
@@ -1,11 +1,11 @@
 const database = require("../database/config");
 
 
-function buscarConectado(){
+function buscarConectado() {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-       instrucaoSql = `
+        instrucaoSql = `
        SELECT 
     valorRegistro,
     CAST(COUNT(*) * 100 / (SELECT COUNT(*) FROM registro WHERE fkComponente = 16) AS DECIMAL(10, 4)) AS porcentagem
@@ -16,20 +16,20 @@ WHERE
 GROUP BY 
     valorRegistro;
    `
-   } else {
-       console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-       return
-   }
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
 
-   console.log("Executando a instrução SQL: \n" + instrucaoSql);
-   return database.executar(instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 
-function buscarDesconectado(){
+function buscarDesconectado() {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-       instrucaoSql = `
+        instrucaoSql = `
        SELECT 
     (COUNT(CASE WHEN valorRegistro = 0 THEN 1 END) * 100 / COUNT(*)) AS porcentagem,
     (SELECT valorRegistro 
@@ -53,35 +53,22 @@ FROM
 WHERE 
     fkComponente = 16;
    `
-   } else if (process.env.AMBIENTE_PROCESSO == "producao") {
-    instrucaoSql = `
-    SELECT 
- (COUNT(CASE WHEN valorRegistro = 0 THEN 1 END) * 100 / COUNT(*)) AS porcentagem,
- (SELECT valorRegistro 
-  FROM registro 
-  WHERE fkComponente = 485 
-  ORDER BY dataRegistro DESC 
-  LIMIT 1) AS ultimovalor,
- (SELECT COUNT(*) 
-  FROM registro 
-  WHERE fkComponente = 485
-    AND valorRegistro = 2 
-    AND DATE(dataRegistro) = CURDATE() - INTERVAL 1 DAY) AS ultimodia,
- (SELECT COUNT(*) 
-  FROM registro 
-  WHERE fkComponente = 485 
-    AND valorRegistro = 2 
-    AND dataRegistro >= CURDATE() - INTERVAL 7 DAY 
-    AND dataRegistro <= CURDATE()) AS ultimasemana
-FROM 
- registro
-WHERE 
- fkComponente = 485;
+    } else if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `
+    
+SELECT
+CONVERT(VARCHAR, USBconectado.dataInicio, 103) + ' às ' + CONVERT(VARCHAR, USBconectado.dataInicio, 108) AS dataInicio,
+CONVERT(VARCHAR, USBconectado.dataFinal, 103) + ' às ' + CONVERT(VARCHAR, USBconectado.dataFinal, 108) AS dataFinal,
+servidor.nome
+FROM USBconectado
+INNER JOIN servidor ON USBconectado.fkServidor = servidor.idServidor
+WHERE USBconectado.fkServidor = 2
+ORDER BY USBconectado.dataFinal DESC;
 `
-}
+    }
 
-   console.log("Executando a instrução SQL: \n" + instrucaoSql);
-   return database.executar(instrucaoSql);
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
 }
 function buscarUSB(fkServidor) {
 
@@ -95,7 +82,7 @@ function buscarUSB(fkServidor) {
     return database.executar(instrucaoSql);
 }
 
-module.exports = {        
+module.exports = {
     buscarDesconectado,
     buscarConectado,
     buscarUSB
