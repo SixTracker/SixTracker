c349bd769e4ed4c43aff0c5509337c69a38b5eee
lyfdl <livia.lanes@sptech.school>
2023-12-11 12:53:15 -0300

    mudanças da apresentação

--- Site/src/models/graficosLiviaModel.js
+++ Site/src/models/graficosLiviaModel.js
@@ -31,27 +31,23 @@ function buscarDesconectado(){
     if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
        instrucaoSql = `
        SELECT 
-    (COUNT(CASE WHEN valorRegistro = 0 THEN 1 END) * 100 / COUNT(*)) AS porcentagem,
-    (SELECT valorRegistro 
-     FROM registro 
-     WHERE fkComponente = 16 
-     ORDER BY dataRegistro DESC 
-     LIMIT 1) AS ultimovalor,
-    (SELECT COUNT(*) 
-     FROM registro 
-     WHERE fkComponente = 16 
-       AND valorRegistro = 2 
-       AND DATE(dataRegistro) = CURDATE() - INTERVAL 1 DAY) AS ultimodia,
-    (SELECT COUNT(*) 
-     FROM registro 
-     WHERE fkComponente = 16 
-       AND valorRegistro = 2 
-       AND dataRegistro >= CURDATE() - INTERVAL 7 DAY 
-       AND dataRegistro <= CURDATE()) AS ultimasemana
-FROM 
-    registro
-WHERE 
-    fkComponente = 16;
+    (COUNT(CASE WHEN valorRegistro = 0 THEN 1 END) * 100.0 / COUNT(*)) AS porcentagem,
+    (SELECT valorRegistro
+        FROM registro
+        WHERE fkComponente = 16
+        ORDER BY dataRegistro DESC
+        LIMIT 1) AS ultimovalor,
+    (SELECT COUNT(*)
+        FROM USB
+        WHERE fkComponente = 16
+            AND DATE(dataInicio) = CURDATE()) AS ultimodia,
+    (SELECT COUNT(*)
+        FROM USB
+        WHERE fkComponente = 16
+            AND DATE(dataInicio) BETWEEN DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND CURDATE()) AS ultimasemana
+FROM registro
+WHERE fkComponente = 16;
+
    `
    } else if (process.env.AMBIENTE_PROCESSO == "producao") {
     instrucaoSql = `
@@ -85,10 +81,10 @@ WHERE
 }
 function buscarUSB(fkServidor) {
 
-    instrucaoSql = `SELECT DATE_FORMAT(USBconectado.dataInicio, '%d/%m/%Y às %H:%i:%s') AS dataInicio, DATE_FORMAT(usb.dataFinal, '%d/%m/%Y às %H:%i:%s') AS dataFinal, servidor.nome
-    FROM USBconectado JOIN servidor ON USBconectado.fkServidor = servidor.idServidor
-    WHERE USBconectado.fkServidor = ${fkServidor}
-    ORDER BY USBconectado.dataFinal DESC;`;
+    instrucaoSql = `SELECT DATE_FORMAT(usb.dataInicio, '%d/%m/%Y às %H:%i:%s') AS dataInicio, DATE_FORMAT(usb.dataFinal, '%d/%m/%Y às %H:%i:%s') AS dataFinal, servidor.nome
+    FROM usb JOIN servidor ON usb.fkServidor = servidor.idServidor
+    WHERE usb.fkServidor = ${fkServidor}
+    ORDER BY usb.dataFinal DESC;`;
 
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
